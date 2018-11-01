9932a228fbb7da9b972bb43ad18febaaf4bfbd5a
AliNisarAhmed <ali.nisar87@gmail.com>
2018-11-01 21:40:08 +0500

    using Context API

---
+++ src/SearchBox.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { ANIMALS } from 'petfinder-client';
+import SearchContext from './SearchContext';
+
+class SearchBox extends React.Component {
+  
+  render() {
+    return (
+      <SearchContext.Consumer>
+        {
+          context => (
+            <div className="search-params">
+              <label htmlFor="location">
+                Location
+                <input 
+                  onChange={context.handleLocationChange}
+                  type="text"
+                  id="location"
+                  value={context.location}
+                  placeholder="Location"
+                  />
+              </label>
+              <label htmlFor="animal">
+                Animal
+                <select
+                id="animal"
+                value={context.animal}
+                onChange={context.handleAnimalChange}
+                onBlur={context.handleAnimalChange}
+                >
+                  <option />
+                  {
+                    ANIMALS.map(animal => (
+                      <option 
+                        key={animal} 
+                        value={animal}
+                      >{animal}</option>
+                    ))
+                  }
+                </select>
+              </label>
+              <label htmlFor="breed">
+                  Breed
+                  <select 
+                    id="breed"
+                    value={context.breed}
+                    onChange={context.handleBreedChange}
+                    onBlur={context.handleBreedChange}
+                    disabled={!context.breeds.length}
+                  >
+                    <option />
+                    {
+                      context.breeds.map(breed => (
+                        <option
+                          key={breed}
+                          value={breed}
+                        >{breed}</option>
+                      ))
+                    }
+                  </select>
+              </label>
+              <button>Submit</button>
+            </div>
+          )
+        }
+        
+      </SearchContext.Consumer>
+    );
+  }
+}
+
+export default SearchBox;
